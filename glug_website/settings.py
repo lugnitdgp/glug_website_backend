ce5c00d05809c65f44a7c968f9c6ca76f366b844
arc9693 <archana.choudhary.9693@gmail.com>
2018-06-22 17:22:05 +0530

    added CORS

--- glug_website/settings.py
+++ glug_website/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+ 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'glug_website.urls'
 
 TEMPLATES = [
