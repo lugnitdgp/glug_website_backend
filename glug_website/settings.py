8e557fb35c60d90be3b465883b7da6011427fb26
SDX <realsdx0@gmail.com>
2018-08-23 00:48:01 +0530

    changed Media root

--- glug_website/settings.py
+++ glug_website/settings.py
@@ -162,4 +162,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'main', 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
