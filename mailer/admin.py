97bfbaab0184efd61744bc04f5115b2c137dcca9
realsdx <realsdx@protonmail.com>
2019-12-09 02:09:23 +0530

    [UPDATE] Major improvements on mailer

--- mailer/admin.py
+++ mailer/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from mailer.models import MailSent
 
-# Register your models here.
+class MailSentAdmin(admin.ModelAdmin):
+    readonly_fields = [f.name for f in MailSent._meta.get_fields()]
+
+    def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser and request.method != 'POST'
+
+admin.site.register(MailSent, MailSentAdmin)
