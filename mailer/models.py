a83ce1e04d3aae359bf626bbc699140d66c500b3
realsdx <realsdx@protonmail.com>
2019-12-08 16:49:45 +0530

    [ADD] basic email client

--- mailer/models.py
+++ mailer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.mail.message import EmailMessage
 
-# Create your models here.
+
+class MailSent(models.Model):
+    subject = models.CharField(max_length=1024)
+    body = models.TextField(blank=True, null=True)
+    from_email = models.CharField(max_length=512)
+    to = models.CharField(max_length=1024)
+    headers = models.TextField(blank=True, null=True)
+    attachment = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.subject
