85acff1948b6829c4fa9094ce5d496c64a630402
realsdx <realsdx@protonmail.com>
2019-12-09 17:57:23 +0530

    [UPADTE] exception handling improvemnt in mailer

--- mailer/views.py
+++ mailer/views.py
@@ -7,13 +7,21 @@ from django.core.mail import EmailMultiAlternatives
 from django.contrib import messages
 from django.urls import reverse
 from django.utils import timezone
+from django.conf import settings
 
 from html2text import html2text
 
+def check_mail_config():
+    if hasattr(settings, 'EMAIL_HOST_USER') and hasattr(settings, 'EMAIL_HOST_PASSWORD'):
+        if settings.EMAIL_HOST_USER != 'None' and settings.EMAIL_HOST_PASSWORD != 'None':
+            return True
+    return False
+
 @login_required
 def index(req):
+    conf_check = check_mail_config()
     history = MailSent.objects.all()
-    return render(req, 'mailer/index.html',{'history':history})
+    return render(req, 'mailer/index.html',{'history':history, 'check':conf_check})
 
 @login_required
 def compose_mail(req):
@@ -35,19 +43,23 @@ def send_mail(req):
 
         msg = EmailMultiAlternatives(subject, text_msg, from_email, [to])
         msg.attach_alternative(html_msg, "text/html")
-        msg.send()
 
-        # log the sent email
-        m = MailSent(subject= subject, body=text_msg, from_email=from_email, to=to, sent_by=req.user, time=timezone.now())
-        m.save()
+        try:
+            msg.send()
 
-        messages.add_message(
-            req, messages.SUCCESS, 'Email successfully sent')
+            # log the sent email
+            m = MailSent(subject= subject, body=text_msg, from_email=from_email, to=to, sent_by=req.user, time=timezone.now())
+            m.save()
+
+            messages.add_message(
+                req, messages.SUCCESS, 'Email successfully sent')
+        except Exception as e:
+            print("ERROR in SMTP :: ", e)
+            messages.add_message(
+                req, messages.ERROR, 'An error occured. Check configuration again.')
+        
         return HttpResponseRedirect('/mail/compose/')
 
     else:
         form = MailComposeForm()
         return HttpResponseRedirect('/mail/compose/',{'form':form})
-
-def check_mail_config():
-    pass
